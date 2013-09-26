04435f40305da462c5570588a88f04a117e21d57
Roger Steve Ruiz <roger.ruiz@rokkan.com>
2013-09-26 12:55:40 -0400

    Update paths

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 $(function () {
 
-  $.get('../data.json', function (d) {
+  $.get('./data.json', function (d) {
     setup(d);
   });
 
   function setup (d) {
-    $.get('../nav.hogan', function (t) {
+    $.get('./nav.hogan', function (t) {
       var template = Hogan.compile(t);
       var rendered = template.render(d);
       $('.coffin').append(rendered);
     });
-    $.get('../article.hogan', function (t) {
+    $.get('./article.hogan', function (t) {
       var template = Hogan.compile(t);
       var rendered = template.render(d);
       $('.page').append(rendered);
