420e974a36097693156db199357a76fa3c93a7a3
Roger Steve Ruiz <roger.ruiz@rokkan.com>
2013-09-26 12:50:19 -0400

    Initial setup for data.json build

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function () {
+
+  $.get('../data.json', function (d) {
+    setup(d);
+  });
+
+  function setup (d) {
+    $.get('../nav.hogan', function (t) {
+      var template = Hogan.compile(t);
+      var rendered = template.render(d);
+      $('.coffin').append(rendered);
+    });
+    $.get('../article.hogan', function (t) {
+      var template = Hogan.compile(t);
+      var rendered = template.render(d);
+      $('.page').append(rendered);
+    });
+  }
+});
